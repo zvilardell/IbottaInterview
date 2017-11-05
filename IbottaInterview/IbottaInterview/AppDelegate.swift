163a86ea419c9153fbe9ce8cb73c63eb1bbbc42e
Zach Vilardell <szvila01@louisville.edu>
2017-11-05 16:07:29 -0500

    set window background color to white

--- IbottaInterview/IbottaInterview/AppDelegate.swift
+++ IbottaInterview/IbottaInterview/AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         //setup window and root view controller
         window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = UIColor.white
         //begin with Offers page visible
         navigationController.pushViewController(OffersViewController(), animated: false)
         window?.rootViewController = navigationController
